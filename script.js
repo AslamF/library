5bb541eb2dbd21cab649720dbadf6257c6f18906
Fraz Aslam <faslam7646@gmail.com>
2023-01-03 19:11:28 -0500

    need to debug and add read button feature

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-const myLibrary = [
-
-];
-
-
-function Book(name, author, pages) {
-    this.name = name,
-    this.author = author,
-    this.pages = pages
-    
-};
-
-
-function createBookObject(name, author, pages){
-  book = new Book(name, author, pages);
-}
-
-
 const newBookButton = document.querySelector(".newBook");
 const cards = document.querySelector(".cards");
-const form = document.querySelector("#bookInfo");
 const popup = document.querySelector(".popup");
+const popupWrapper = document.querySelector(".popupWrapper");
+const cancel =document.querySelector("#cancel");
+const submit = document.querySelector("#submit");
 
 let newCard;
 let bookName;
@@ -27,83 +11,127 @@ let bookAuthor;
 let number;
 let del;
 let read;
+let bookTitle;
+let authorName;
+let pageNumber;
+let bookRead;
 
 
+// array for cards
+const myLibrary = [
+
+];
+
+// Object Constructor 
+function Book(name, author, pages, read) {
+    this.name = name,
+    this.author = author,
+    this.pages = pages,
+    this.read = read 
+};
+
+// Function for Object Constructor
+function createBookObject(name, author, pages, read){
+  book = new Book(name, author, pages, read);
+}
+
+
+
+
+
+
+function createCard(){
+  newCard = document.createElement("div");
+  newCard.classList.add("newCard");
+  bookName = document.createElement("div");
+  bookName.classList.add("bookName");
+  bookAuthor = document.createElement("div")
+  bookAuthor.classList.add("bookAuthor");
+  number = document.createElement("div")
+  number.classList.add("pageNumber");
+
+  const buttonGroup = document.createElement("div");
+  buttonGroup.classList.add("buttonGroup");
+
+  read = document.createElement("button");
+  read.classList.add("read");
+  
+  del = document.createElement("button");
+  del.classList.add("delete");
+  del.textContent = "delete";
+  
+  newCard.appendChild(bookName);
+  newCard.appendChild(bookAuthor);
+  newCard.appendChild(number);
+  buttonGroup.appendChild(read);
+  buttonGroup.appendChild(del);
+  newCard.appendChild(buttonGroup);
+
+}
 
 
 // Loop Array, Create Card with array element
 function arrayLoop(){
 for(let i = 0; i < myLibrary.length; i++){
-    newCard = document.createElement("div");
-    newCard.classList.add("newCard");
-    bookName = document.createElement("div");
-    bookName.classList.add("bookName");
-    bookAuthor = document.createElement("div")
-    bookAuthor.classList.add("bookAuthor");
-    number = document.createElement("div")
-    number.classList.add("pageNumber");
+    createCard();
 
-    const buttonGroup = document.createElement("div");
-    buttonGroup.classList.add("buttonGroup");
-
-    read = document.createElement("button");
-    read.classList.add("read");
-    read.textContent = "read";
-
-    del = document.createElement("button");
-    del.classList.add("delete");
-    del.textContent = "delete";
-    
-    newCard.appendChild(bookName);
-    newCard.appendChild(bookAuthor);
-    newCard.appendChild(number);
-    buttonGroup.appendChild(read);
-    buttonGroup.appendChild(del);
-    newCard.appendChild(buttonGroup);
+    if(myLibrary[i].read === true){
+      read.textContent = "READ";
+    }
+    else{
+      read.textContent = "NOT READ";
+    }
 
     bookName.textContent = `"${myLibrary[i].name}"`;
     bookAuthor.textContent = `${myLibrary[i].author}`;
-    number.textContent = `${myLibrary[i].pages}`;
+    number.textContent = `${myLibrary[i].pages} - pages`;
     
     cards.appendChild(newCard);
 
     del.addEventListener("click", ()=>{
-      console.log("tree");
       myLibrary.splice(i, 1);
       cards.textContent = "";
       arrayLoop();
     })
-    
+
+    if(myLibrary[i].read === true){
+      read.classList.add("active");
+    }
     
 }}
 
-const submit = document.querySelector("#submit");
-
-
-
-let bookTitle;
-let authorName;
-let pageNumber;
-
-
-
+// When click submit, CREATE OBJECT THEN ADD INTO ARRAY
 submit.addEventListener("click", (event)=>{
   event.preventDefault();
   bookTitle = document.querySelector("#title").value;
   authorName = document.querySelector("#author").value;
   pageNumber = document.querySelector("#pages").value;
-  createBookObject(bookTitle, authorName, pageNumber);
+  if(document.querySelector("#read").checked === true){
+    bookRead = true;
+  }
+  else{
+    bookRead = false;
+  }
+
+  createBookObject(bookTitle, authorName, pageNumber, bookRead);
   myLibrary.push(book);
   cards.textContent = "";
   arrayLoop();
   document.querySelector("#bookInfo").reset();
   popup.classList.remove("active");
+  popupWrapper.classList.remove("active");
+  
 
 });
 
 newBookButton.addEventListener("click", ()=> {
     popup.classList.add("active");
+    popupWrapper.classList.add("active");
+    
     
 
 })
-
+cancel.addEventListener("click", ()=>{
+  popup.classList.remove("active")
+  popupWrapper.classList.remove("active")
+})
